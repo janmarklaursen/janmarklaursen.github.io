6b90b9244a9635f9b2423447cb8a687c11cec68f
janmarklaursen <45092364+janmarklaursen@users.noreply.github.com>
2024-02-10 18:29:21 +0100

    names of imges added

--- js/app.js
+++ js/app.js
@@ -3,12 +3,12 @@ const coffees = [
   { name: "Perspiciatis", image: "images/coffee1.jpg" },
   { name: "Voluptatem", image: "images/coffee2.jpg" },
   { name: "Explicabo", image: "images/coffee3.jpg" },
-  { name: "Rchitecto", image: "images/coffee4.jpg" },
-  { name: " Beatae", image: "images/coffee5.jpg" },
-  { name: " Vitae", image: "images/coffee6.jpg" },
-  { name: "Inventore", image: "images/coffee7.jpg" },
-  { name: "Veritatis", image: "images/coffee8.jpg" },
-  { name: "Accusantium", image: "images/coffee9.jpg" },
+  { name: "Rchitecto", image: "images/coffee1.jpg" },
+  { name: " Beatae", image: "images/coffee2.jpg" },
+  { name: " Vitae", image: "images/coffee3.jpg" },
+  { name: "Inventore", image: "images/coffee1.jpg" },
+  { name: "Veritatis", image: "images/coffee2.jpg" },
+  { name: "Accusantium", image: "images/coffee3.jpg" },
 ]
 
 const showCoffees = () => {
